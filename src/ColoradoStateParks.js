f7de1a5f610569d4c9adce2d59a01cf98cbe0a4c
Asma <as1588ma@gmail.com>
2022-06-19 19:24:57 -0400

    complete homework

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,62 @@
+// import React from "react";
+// import MesaVerde from "./parks/MesaVerde";
+
+// function ColoradoStateParks() {
+//   return (
+//     <div>
+//       <MesaVerde />
+//     </div>
+//   );
+// }
+
+// export default ColoradoStateParks;
+
+// import { trees, wildlife } from "./parks/RockyMountain";
+
+// console.log(trees);
+// // => "Aspen and Pine"
+
+// wildlife();
+
+// src/ColoradoStateParks.js
+// import * as RMFunctions from "./parks/RockyMountain";
+
+// console.log(RMFunctions.trees);
+// // => "Aspen and Pine"
+
+// RMFunctions.wildlife();
+// // => "Elk, Bighorn Sheep, Moose"
+
+// RMFunctions.elevation();
+// // => Attempted import error
+
+// import { trees, wildlife } from "./parks/RockyMountain";
+
+// console.log(trees);
+// // > "Aspen and Pine"
+
+// wildlife();
+
+// import {
+//   trees as parkTrees,
+//   wildlife as parkWildlife,
+// } from "./parks/RockyMountain";
+
+// console.log(parkTrees);
+// // > "Aspen and Pine"
+
+// parkWildlife();
+
+// src/ColoradoStateParks.js
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
